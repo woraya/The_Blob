3ddfb1e9aa3644ca3e494c5988c98a1221bd9f95
Woraya : Mint <wb743@nyu.edu>
2017-07-18 13:58:18 -0400

    add Osc Scripts

---
+++ unity/scripts/Osc/NoiseDeform.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var NoiseScale = 0.001;		// 0.5
+var speed = 0.01;			// 0.8
+var recalculateNormals = false;
+
+private var baseVertices : Vector3[];
+private var noise : Perlin;
+
+function Start ()
+{
+    noise = new Perlin ();
+}
+
+function Update () {
+    var mesh : Mesh = GetComponent(MeshFilter).mesh;
+	
+    var intensity = GameObject.Find("OSC Receiver").GetComponent(OSCReceiver).NoiseIntensity;
+
+    var scale = NoiseScale * intensity;
+
+    if (baseVertices == null)
+        baseVertices = mesh.vertices;
+		
+    var vertices = new Vector3[baseVertices.Length];
+	
+    var timex = Time.time * speed + 0.1365143;
+    var timey = Time.time * speed + 1.21688;
+    var timez = Time.time * speed + 2.5564;
+    for (var i=0;i<vertices.Length;i++)
+    {
+        var vertex = baseVertices[i];
+				
+        vertex.x += noise.Noise(timex + vertex.x, timex + vertex.y, timex + vertex.z) * scale;
+        vertex.y += noise.Noise(timey + vertex.x, timey + vertex.y, timey + vertex.z) * scale;
+        vertex.z += noise.Noise(timez + vertex.x, timez + vertex.y, timez + vertex.z) * scale;
+		
+        vertices[i] = vertex;
+    }
+	
+    mesh.vertices = vertices;
+	
+    if (recalculateNormals)	
+        mesh.RecalculateNormals();
+    mesh.RecalculateBounds();
+}
---
+++ unity/scripts/Osc/OSCReceiver.js
@@ -0,0 +1,85 @@
+﻿#pragma strict
+
+public var RemoteIP : String = "127.0.0.1"; //127.0.0.1 signifies a local host (if testing locally
+public var SendToPort : int = 5555; //the port you will be sending from
+public var ListenerPort : int = 2222; //the port you will be listening on
+public var controller : Transform;
+public var controller2 : Transform;
+public var NoiseIntensity : float = 0;
+
+private var val : float;
+
+// the OSC object
+private var osc : Osc;
+private var udp : Udp;
+
+public var floorAnimator : Animator;
+public var ceilAnimator : Animator;
+
+function Start () {
+    //NoiseIntensity = new float;
+    
+    udp = GetComponent("Udp");
+    udp.init(RemoteIP, SendToPort, ListenerPort);
+
+    osc = GetComponent("Osc");
+    osc.init(udp);
+    osc.SetAllMessageHandler(AllMessageHandler);
+}
+
+function Update () {
+    NoiseIntensity = val;
+
+    if( NoiseIntensity >= 40 ){
+
+         floorAnimator.SetBool("squeeze",true);
+         ceilAnimator.SetBool("squeeze",true);
+
+    }
+     else{
+         floorAnimator.SetBool("squeeze",false);
+         ceilAnimator.SetBool("squeeze",false);
+    }
+}
+/*
+public static function Log(message : String) {
+    if(debug === true) { 
+        Debug.Log(msg);
+    }
+}*/
+
+public function AllMessageHandler(msg: OscMessage){
+
+    // log the OSC message
+    Debug.Log(osc.OscMessageToString(msg));
+
+    // message parameters
+    var address = msg.Address;
+    var values = msg.Values;
+
+    // variables to hold the data
+    var group_id : String;
+
+    // index of our world objects
+    var i : int;
+
+    // different actions, based on the address pattern
+    switch (address){
+
+        // FORMAT:  /cursor id group_id x y z x_world y_world z_world 
+        case "/test":
+
+            // extract the data
+            
+            val = values[0];
+
+            // log the data
+
+            Debug.Log(
+                  "Noise Intensity: " + val
+            );
+
+            break;
+
+    }
+}
